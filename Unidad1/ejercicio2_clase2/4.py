962dfc3daa93a4fe84f7ecd12875617f4b7b6f84
amandap2604 <112110565+amandap2604@users.noreply.github.com>
2023-09-03 23:38:11 -0300

    Update 4.py

--- Unidad1/ejercicio2_clase2/4.py
+++ Unidad1/ejercicio2_clase2/4.py
@@ -1,7 +1,12 @@
-import time
+import sys
 inicio = time.time()
-
+sys.setrecursionlimit(10 ** 9)
 def factorial_recursivamente(numero):
-    print("a")
+    if numero == 1 or numero == 0:
+        return 1
+    else:
+        return numero * factorial_recursivamente(numero-1)
+
+print(factorial_recursivamente(10000))
 fin = time.time()
 print(fin-inicio)
